2ea1357f285384210b299a7e1488f2d40ef8a853
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2024-08-02 12:36:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Navbar from './Components/common/Navbar';
 import SubNavbar from './Components/common/Subnavbar';
 import HomePage from './Pages/Home';
 import Footer from './Components/common/Footer';
+import ShopBrand from "./Pages/shopbrand";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-<SubNavbar/>
-<HomePage/>
-<Footer/>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <SubNavbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shopbrand" element={<ShopBrand />} />
+        </Routes>
+        <Footer />
       </div>
+    </Router>
   );
 }
 
