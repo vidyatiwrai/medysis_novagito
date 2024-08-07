8a98643ef3703a12111a941d05f95462cd550bce
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2024-08-07 11:07:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,35 @@ import HomePage from './Pages/Home';
 import Footer from './Components/common/Footer';
 import ShopBrand from "./Pages/shopbrand";
 import Dental from './Pages/Dental';
+import Featured from "./Pages/Featured"
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <SubNavbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shopbrand" element={<ShopBrand />} />
+          <Route path="/dental" element={<Dental/>} />
+          <Route path="/featured" element={<Featured/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './Components/common/Navbar';
+import SubNavbar from './Components/common/Subnavbar';
+import HomePage from './Pages/Home';
+import Footer from './Components/common/Footer';
+import ShopBrand from "./Pages/shopbrand";
+import Dental from './Pages/Dental';
 
 function App() {
   return (
