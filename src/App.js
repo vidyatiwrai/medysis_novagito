009c0a8e0526fc4490342e0208d1aef64fe02a90
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2024-08-09 12:24:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './Components/common/Footer';
 import ShopBrand from "./Pages/shopbrand";
 import Dental from './Pages/Dental';
 import Featured from "./Pages/Featured"
+import Cartpage from './Pages/Cartpage';
 
 function App() {
   return (
@@ -19,33 +20,8 @@ function App() {
           <Route path="/shopbrand" element={<ShopBrand />} />
           <Route path="/dental" element={<Dental/>} />
           <Route path="/featured" element={<Featured/>} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+        <Route path="/cartpage" element={<Cartpage/>} />
 
-export default App;
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Navbar from './Components/common/Navbar';
-import SubNavbar from './Components/common/Subnavbar';
-import HomePage from './Pages/Home';
-import Footer from './Components/common/Footer';
-import ShopBrand from "./Pages/shopbrand";
-import Dental from './Pages/Dental';
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <SubNavbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shopbrand" element={<ShopBrand />} />
-          <Route path="/dental" element={<Dental/>} />
         </Routes>
         <Footer />
       </div>
